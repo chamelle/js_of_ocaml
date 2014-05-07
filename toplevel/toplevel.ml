241331589fea03ff2af46c1542986fc2768ef8c4
Thomas HUET <thomas.h@ens.fr>
2014-05-07 23:53:23 +0200

    Remove duplicated line

--- toplevel/toplevel.ml
+++ toplevel/toplevel.ml
@@ -132,7 +132,6 @@ end = struct
     Hashtbl.add Toploop.directive_table "disable" (Toploop.Directive_string Option.Optim.disable);
     Hashtbl.add Toploop.directive_table "debug_on" (Toploop.Directive_string Option.Debug.enable);
     Hashtbl.add Toploop.directive_table "debug_off" (Toploop.Directive_string Option.Debug.disable);
-    Hashtbl.add Toploop.directive_table "debug_off" (Toploop.Directive_string Option.Debug.disable);
     Hashtbl.add Toploop.directive_table "tailcall" (Toploop.Directive_string (fun s ->
       let x = Option.Tailcall.of_string s in
       Option.Tailcall.set x));
